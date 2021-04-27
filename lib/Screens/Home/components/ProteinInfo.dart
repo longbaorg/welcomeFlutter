b6a1766479f35d97c5b1d64bef72dc0a1e5ff5be
longbao <2590595662@qq.com>
2021-04-27 22:38:48 +0800

    init
    (cherry picked from commit cad0c4605482758d462c08be5a2f00d19f27d7df)

---
+++ lib/Screens/Home/components/ProteinInfo.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/cupertino.dart';
+
+class ProteinInfo extends StatefulWidget {
+  ProteinInfo({Key key, @required this.id}) : super(key: key);
+  final String id;
+
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    print(id+"88888888888888");
+    throw UnimplementedError();
+  }
+
+}
